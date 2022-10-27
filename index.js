383e365abf9f4da7d606751db00aaeca0f3c689f
Lourdhemani <116826697+Lourdhemani@users.noreply.github.com>
2022-10-27 21:53:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 
+app.use("/", (req, res) => {
+  res.send({
+    status: 200,
+    message: "Hacktober fest!"
+  })
+})
+
 app.listen(4000, () => {
   console.log("Server listening on port 4000");
 })
